f90894801699222abdbf28c5ed39455bacc95213
Greg (Tommy) Thompson <gthompson333@gmail.com>
2023-11-26 14:45:59 -0800

    Added additional Firebase configuration to support iOS. Also, Firebase Options file.

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:jasper_health_weight_tracker/src/ui/screens/weight_list.dart';
 
+import 'firebase_options.dart';
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
